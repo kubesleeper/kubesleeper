c1511b68bdf77c29836d1af2b3a423facea30bb5
eloi-menaud <eloi-menaud@users.noreply.github.com>
2025-11-22 19:07:06 +0000

    deploy: f7ae47c026bebea1de808369552fd18e7810978a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-75f2efeb.js',
+                path_to_root + 'searchindex-cedbfc8f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
