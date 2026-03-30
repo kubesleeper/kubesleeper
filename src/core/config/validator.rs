b8987b48b92daad96fe53a012ec58cebc639657e
eloi-menaud <eloimenaud@proton.me>
2026-03-30 23:39:47 +0200

    feat: rework validation function for better logs

--- src/core/config/validator.rs
+++ src/core/config/validator.rs
@@ -6,28 +6,37 @@ use std::fmt::{Display};
 
 #[derive(Debug,Clone)]
 pub enum ResourceType {
-    Service,
-    Deployment,
+    Services,
+    Deployments,
+}
+impl std::fmt::Display for ResourceType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", format!("{:?}", self).to_lowercase())
+    }
 }
 
-#[derive(Debug)]
-pub struct ConflictError{
-    from_group_name: String,
-    from_id: Identifier,
-    to_group_name: String,
-    to_id: Identifier,
-    resource_type:ResourceType
-}
-impl Display for ConflictError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Field '{}.{:?}.{}' collide with '{}.{:?}.{}'", 
-            self.from_group_name,
-            self.resource_type,
-            self.from_id,
-            self.to_group_name,
-            self.resource_type,
-            self.to_id
-        )
+
+
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConflictError {
+    
+    #[error("'{from_group_name}.{resource_type}.{from_id}' and '{to_group_name}.{resource_type}.{to_id}' target same resource(s)")]
+    ExpliciteError{
+        from_group_name: String,
+        from_id: Identifier,
+        to_group_name: String,
+        to_id: Identifier,
+        resource_type:ResourceType
+    },
+    
+    #[error("'{from_group_name}.{resource_type}.{from_id}' already covers '{to_group_name}.{resource_type}.{to_id}' resource")]
+    WildcardError{
+        from_group_name: String,
+        from_id: Identifier,
+        to_group_name: String,
+        to_id: Identifier,
+        resource_type:ResourceType
     }
 }
 
@@ -44,99 +53,89 @@ pub enum ValidatorError {
 ///   group1:
 ///     deployments:
 ///       - a/b ─────┐
-///   group2:        | duplicates, explicite
-///     deployments: |  
+///   group2:        | explicite duplicates
+///     deployments: |
 ///       - a/b ─────┘
+///
+/// ─────────────────────
 /// 
 /// groups:
 ///   group1:
 ///     deployments:
 ///       - a/* ─────┐
-///   group2:        | duplicates, implicite : a/* already cover a/b
-///     deployments: |  
+///   group2:        | implicite duplicates (a/* already covers a/b)
+///     deployments: |
 ///       - a/b ─────┘
-/// 
+///
 pub fn check_collisions(config: &Config, resource_type: ResourceType) -> Vec<ConflictError> {
     let mut errors : Vec<ConflictError>= Vec::new();
-    let mut reported_pairs = HashSet::new();
     
+    // flatten config, to be in O(n2) instead of O(n4)
     let all_ids: Vec<(&String, &Identifier)> = match resource_type {
-        ResourceType::Service => {
+        ResourceType::Services => {
             config.groups
                 .iter()
-                .flat_map(|(name, group)| group.services.iter().map(move |id| (name, id)))
+                .flat_map(|(group)| group.services.iter().map(move |id| (&group.name, id)))
                 .collect()
         },
-        ResourceType::Deployment => {
+        ResourceType::Deployments => {
             config.groups
                 .iter()
-                .flat_map(|(name, group)| group.deployments.iter().map(move |id| (name, id)))
+                .flat_map(|(group)| group.deployments.iter().map(move |id| (&group.name, id)))
                 .collect()
         }
     };
-    
+
+    for i in 0..all_ids.len() {
+        for j in i + 1..all_ids.len() {
+            let (g1, id1) = all_ids[i];
+            let (g2, id2) = all_ids[j];
+
+            if id1.namespace == id2.namespace && id1.name == id2.name {
+                errors.push( ConflictError::ExpliciteError{
+                    from_group_name: format!("{}",g1),
+                    from_id: id1.to_owned().to_owned(),
+                    to_group_name: format!("{}",g2),
+                    to_id: id2.to_owned().to_owned(),
+                    resource_type: resource_type.clone()
+                });
+            }
+        }
+    }
     
     
     
     let wildcards: Vec<_> = all_ids.iter().filter(|(_, id)| id.name.is_star()).collect();
     let explicits: Vec<_> = all_ids.iter().filter(|(_, id)| !id.name.is_star()).collect();
+    
 
-    // --- PHASE 1 : Priorité aux Wildcards (*) ---
-    // Un "*" peut entrer en collision avec un autre "*" ou un nom explicite
     for (g_w, id_w) in &wildcards {
-        for (g_any, id_any) in &all_ids {
-            // Ne pas se comparer soi-même
-            if std::ptr::eq(*g_w, *g_any) && std::ptr::eq(*id_w, *id_any) { continue; }
+        for (g_e, id_e) in &explicits {
 
-            if id_w.namespace == id_any.namespace {
-                if !reported_pairs.contains(&(id_w, id_any)) && !reported_pairs.contains(&(id_any, id_w)) {
-                    errors.push( ConflictError{
-                        from_group_name: format!("{}",g_w),
-                        from_id: id_w.to_owned().to_owned(),
-                        to_group_name: format!("{}",g_any),
-                        to_id: id_any.to_owned().to_owned(),
-                        resource_type: resource_type.clone()
-                    });
-                    reported_pairs.insert((id_w, id_any));
-                }
-            }
-        }
-    }
-
-    // --- PHASE 2 : Collisions Explicites (sans les *) ---
-    for i in 0..explicits.len() {
-        for j in i + 1..explicits.len() {
-            let (g1, id1) = explicits[i];
-            let (g2, id2) = explicits[j];
-
-            if id1.namespace == id2.namespace && id1.name == id2.name {
-                // On vérifie si cet ID n'a pas déjà été "couvert" par un message de Phase 1
-                // (Optionnel selon ta préférence, ici on ne rapporte le doublon que s'il est "nu")
-                if !reported_pairs.iter().any(|(w, any)| std::ptr::eq(*any, id1) || std::ptr::eq(*any, id2)) {
-                    errors.push( ConflictError{
-                        from_group_name: format!("{}",g1),
-                        from_id: id1.to_owned().to_owned(),
-                        to_group_name: format!("{}",g2),
-                        to_id: id2.to_owned().to_owned(),
-                        resource_type: resource_type.clone()
-                    });
-                }
+            if id_w.namespace == id_e.namespace {
+                errors.push( ConflictError::WildcardError{
+                    from_group_name: format!("{}",g_w),
+                    from_id: id_w.to_owned().to_owned(),
+                    to_group_name: format!("{}",g_e),
+                    to_id: id_e.to_owned().to_owned(),
+                    resource_type: resource_type.clone()
+                });
             }
         }
     }
     errors
 }
 
-
 pub fn validator(config: &Config) -> Result<(), ValidatorError> {
-    
-    let mut errors = check_collisions(config, ResourceType::Deployment);
-    errors.append(&mut check_collisions(config, ResourceType::Service));
-    
+    let mut errors = check_collisions(config, ResourceType::Deployments);
+    errors.append(&mut check_collisions(config, ResourceType::Services));
+
     match errors.is_empty() {
         true => Ok(()),
         false => Err(ValidatorError::ConflictErrors(errors))
     }
 
 }
+
+
 
